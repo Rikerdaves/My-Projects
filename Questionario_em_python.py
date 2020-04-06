4178daaa5219481ea6a268f3b43b5164f243e59f
Rikerdaves <Rikerdaves.alencar@gmail.com>
2020-04-06 07:49:57 -0300

    Create Questionario_em_python.py

---
+++ Questionario_em_python.py
@@ -0,0 +1,20 @@
+def pergunta1():
+    print ('''Questão 1: (Enem/2013) É preciso ressaltar que, de todas as capitanias brasileiras, 
+    Minas era a mais urbanizada. Não havia ali hegemonia de um ou dois grandes centros. 
+    A região era repleta de vilas e arraiais, grandes e pequenos, em cujas ruas muita gente circulava.
+
+    a) apropriação cultural diante das influências externas.
+    b) produção manufatureira diante do exclusivo comercial.
+    c) insubordinação religiosa diante da hierarquia eclesiástica.
+    d) fiscalização estatal diante das particularidades econômicas.
+    e)  autonomia administrativa diante das instituições metropolitanas.
+    ''')
+    resp = input ("Escola a alternativa correta: ")
+    if resp == ("d"):
+        print ("Você acertou")
+
+    else:
+        print (" você errou")
+
+print('Questionário de História\n')
+pergunta1()        
